12191b418bd9c277125afa267c223cbecd042762
jaemoonnlee <jaemoon.n.lee@gmail.com>
2021-08-03 12:42:23 +0900

    use mysql

--- test_django/mysite/mysite/settings.py
+++ test_django/mysite/mysite/settings.py
@@ -75,18 +75,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.oracle',
-    #     'NAME': 'db.oracle',# DB SCHEMA(NAME)
-    #     'USER': 'mydatabaseuser',# ID
-    #     'PASSWORD': 'mypassword',# PASSWORD
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'py_mysql',# DB SCHEMA(NAME)
+        'USER': 'root',# ID
+        'PASSWORD': '1234',# PASSWORD
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
 }
 
 
